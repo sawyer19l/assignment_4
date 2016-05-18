cad386cc1f947951720714add01775d745a37d6c
sawyer19l <vedran.boricic@gmail.com>
2016-05-18 01:07:14 -0400

    "finished assignment"

--- Assignments/version2/module4-solution-starter/harder/script.js
+++ Assignments/version2/module4-solution-starter/harder/script.js
@@ -32,10 +32,10 @@ WARNING!!! WARNING!!!
 // STEP 1:
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 (function () {
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
